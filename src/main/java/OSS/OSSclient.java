33126d90320c332213e4a2ef8c424af8bf6c2b5e
scp-WFZ <1157264534@qq.com>
2022-04-10 12:07:48 +0800

    Update

--- src/main/java/OSS/OSSclient.java
+++ src/main/java/OSS/OSSclient.java
@@ -3,6 +3,7 @@ package OSS;
 import Config.Config;
 
 import com.aliyun.oss.*;
+import com.aliyun.oss.model.GetObjectRequest;
 import com.aliyun.oss.model.PutObjectRequest;
 
 import java.io.*;
@@ -14,20 +15,22 @@ public class OSSclient {
     private static final String EndPoint = Config.getEndPoint();
     private static final String AccessKeyId = Config.getAccessKeyId();
     private static final String AccessKeySecret = Config.getAccessKeySecret();
-    public static OSS ossclient = new OSSClientBuilder().build(EndPoint, AccessKeyId, AccessKeySecret);
+    public static OSS ossClient = new OSSClientBuilder().build(EndPoint, AccessKeyId, AccessKeySecret);
 
     public static void fileUpload(){
         String filename = "Diana.jpg";
         File file = new File("Resource/Diana.jpg");
         PutObjectRequest putObjectRequest = new PutObjectRequest(BucketName, filename, file);
-        ossclient.putObject(putObjectRequest);
+        ossClient.putObject(putObjectRequest);
     }
     public static void fileDownload(){
-
+        String filename = "Diana.jpg";
+        ossClient.getObject(new GetObjectRequest(BucketName,filename), new File("Resource/Diana1.jpg"));
     }
 
     public static void main(String[] args) {
         fileUpload();
-        ossclient.shutdown();
+        fileDownload();
+        ossClient.shutdown();
     }
 }
